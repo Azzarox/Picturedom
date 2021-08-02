723bd200d4fef8640e1c789c0f645e506863d238
azzarox <lenovo700gmz@abv.bg>
2021-08-02 12:45:41 +0300

    added homepage_view which shows the photos

--- Picturedom/Picturedom/photo/views.py
+++ Picturedom/Picturedom/photo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from Picturedom.photo.models import Photo
+
+
+def homepage_photos(request):
+    """
+    Shows all the photos on the homepage, ordered by the most recent posted one
+    """
+    photos = Photo.objects.all().order_by('-posted_at')
+    context = {
+        'photos': photos
+    }
+    return render(request, 'photos/homepage_photos.html', context)
