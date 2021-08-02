e3213b6dc8ce292c3eda34d6277a8c54e49fae7b
azzarox <lenovo700gmz@abv.bg>
2021-08-02 13:12:12 +0300

    added view and url path for creating photo

--- Picturedom/Picturedom/photo/urls.py
+++ Picturedom/Picturedom/photo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from Picturedom.photo.views import homepage_photos
+from Picturedom.photo.views import homepage_photos, create_photo
 
 urlpatterns = (
     path('', homepage_photos, name='homepage photos'),
+    path('photo/create/', create_photo, name='create photo'),
 )
--- Picturedom/Picturedom/photo/views.py
+++ Picturedom/Picturedom/photo/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
+from Picturedom.photo.forms import PhotoForm
 from Picturedom.photo.models import Photo
 
 
@@ -12,3 +14,24 @@ def homepage_photos(request):
         'photos': photos
     }
     return render(request, 'photos/homepage_photos.html', context)
+
+
+@login_required()
+def create_photo(request):
+    """
+    Creates new photo. Automatically assigns the 'posted_by' property to the user.
+    """
+    if request.method == 'POST':
+        form = PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.posted_by = request.user
+            photo.save()
+            return redirect('homepage photos')
+    else:
+        form = PhotoForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'photos/create_photo.html', context)
