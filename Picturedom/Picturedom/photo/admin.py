721eb89e901bfa3fde2ecc50e8bd8659272ebd4f
azzarox <lenovo700gmz@abv.bg>
2021-08-02 10:51:53 +0300

    added models, model migrations, and registered in admin.py

--- Picturedom/Picturedom/photo/admin.py
+++ Picturedom/Picturedom/photo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from Picturedom.photo.models import Comment, CommentLike, CommentDislike, Photo,Category
+
+admin.site.register(Photo)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(CommentLike)
+admin.site.register(CommentDislike)
