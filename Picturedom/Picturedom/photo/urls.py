e126e79a88a7a75b7e6784a4cc608b5f9cd00c1b
azzarox <lenovo700gmz@abv.bg>
2021-08-02 13:40:19 +0300

    added photos comment GET and POST urls patterns

--- Picturedom/Picturedom/photo/urls.py
+++ Picturedom/Picturedom/photo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from Picturedom.photo.views import homepage_photos, create_photo
+from Picturedom.photo.views import homepage_photos, create_photo, photo_comments, add_comment
 
 urlpatterns = (
     path('', homepage_photos, name='homepage photos'),
     path('photo/create/', create_photo, name='create photo'),
+    path('photo/<int:pk>/', photo_comments, name='photo comments'),
+    path('photo/comments/<int:pk>/', add_comment, name='add comment'),
 )
