b019cce7ab0d370b13ee0455baff131b59eab129
azzarox <lenovo700gmz@abv.bg>
2021-08-02 13:39:55 +0300

    added views for GET and POST of photo's comments

--- Picturedom/Picturedom/photo/views.py
+++ Picturedom/Picturedom/photo/views.py
@@ -1,7 +1,8 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-from Picturedom.photo.forms import PhotoForm
+from Picturedom.photo.forms import PhotoForm, PhotoCommentForm
 from Picturedom.photo.models import Photo
 
 
@@ -35,3 +36,38 @@ def create_photo(request):
         'form': form
     }
     return render(request, 'photos/create_photo.html', context)
+
+
+def photo_comments(request, pk):
+    """
+    Shows the 3 most recent comments.
+    Shows the comment text box - GET.
+    """
+    photo = Photo.objects.get(pk=pk)
+    comments = photo.comment_set.all().order_by('-posted_at')[:3]
+    context = {
+        'photo': photo,
+        'comments': comments,
+        'form': PhotoCommentForm(initial={
+            'image_pk': pk
+        }),
+    }
+    return render(request, 'photos/comments.html', context)
+
+
+def add_comment(request, pk):
+    """
+    Adds comment to the image - POST.
+    Checks the user and assigns it to the comment.posted_by
+    """
+    form = PhotoCommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.user = request.user
+        comment.save()
+    else:
+        text = 'You have used forbidden word!'
+        messages.warning(request, text)
+    return redirect('photo comments', pk)
+
+# edit comment only for the user logged in
