b834728f72393c296e6f9e03c2257e3bab2a90b7
azzarox <lenovo700gmz@abv.bg>
2021-08-02 12:22:18 +0300

    added paths

--- Picturedom/Picturedom/urls.py
+++ Picturedom/Picturedom/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Picturedom.photo.urls'))
+    path('', include('Picturedom.photo.urls')),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('Picturedom.auth_app.urls')),
 ]
