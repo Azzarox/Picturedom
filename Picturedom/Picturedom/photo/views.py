461b8e174b5af54c826ce4902f884471f7be2567
azzarox <lenovo700gmz@abv.bg>
2021-08-03 11:09:24 +0300

    added like/dislike comment views

--- Picturedom/Picturedom/photo/views.py
+++ Picturedom/Picturedom/photo/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from Picturedom.photo.forms import PhotoForm, PhotoCommentForm, EditCommentForm
-from Picturedom.photo.models import Photo, Comment
+from Picturedom.photo.models import Photo, Comment, CommentDislike, CommentLike
 
 
 def homepage_photos(request):
@@ -97,3 +97,48 @@ def delete_comment(request, pk):
     image_pk = comment.comment_image.id
     comment.delete()
     return redirect('photo comments', image_pk)
+
+
+@login_required()
+def like_comment(request, pk):
+    """
+    Likes the given comment, if already liked by the user it sends message and doesn't POST.
+    """
+    comment = Comment.objects.get(pk=pk)
+    photo_pk = comment.comment_image.id
+    # checks if there is a like by the user
+    is_liked_by_user = comment.commentlike_set.filter(
+        user=request.user).exists()
+
+    if not is_liked_by_user:
+        likes = CommentLike(
+            comment=comment,
+            user=request.user
+        )
+        likes.save()
+    else:
+        messages.warning(request, 'You have already liked this comment')
+
+    return redirect('photo comments', photo_pk)
+
+
+@login_required()
+def dislike_comment(request, pk):
+    """
+    Dislikes the given comment, if already disliked by the user it sends message and doesn't POST.
+    """
+    comment = Comment.objects.get(pk=pk)
+    photo_pk = comment.comment_image.id
+    # checks if there is a dislike by the user
+    is_disliked_by_user = comment.commentdislike_set.filter(
+        user=request.user).exists()
+
+    if not is_disliked_by_user:
+        dislikes = CommentDislike(
+            comment=comment,
+            user=request.user
+        )
+        dislikes.save()
+    else:
+        messages.warning(request, 'You have already disliked this comment')
+    return redirect('photo comments', photo_pk)
