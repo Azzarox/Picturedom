4030abe63381981ca19b9e595a8d4761281d063e
azzarox <lenovo700gmz@abv.bg>
2021-08-03 12:46:48 +0300

    added profile_user app path to urls

--- Picturedom/Picturedom/urls.py
+++ Picturedom/Picturedom/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Picturedom.photo.urls')),
     path('', include('django.contrib.auth.urls')),
     path('', include('Picturedom.auth_app.urls')),
+    path('', include('Picturedom.profile_user.urls')),
+    path('', include('Picturedom.photo.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
