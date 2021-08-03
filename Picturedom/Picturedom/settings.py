d5a2c7e9dc46999b9b9a4c684138781548fe6f7e
azzarox <lenovo700gmz@abv.bg>
2021-08-03 14:04:30 +0300

    changed debug and added allowed hosts

--- Picturedom/Picturedom/settings.py
+++ Picturedom/Picturedom/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--l!i*%*@nk!bz*#-06nzkasuepqwm16fj)y&eh&rn&w$%$+2+6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
