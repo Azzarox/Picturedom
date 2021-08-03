4314d8a054a5ec57549c78c4e86943588c6babcf
azzarox <lenovo700gmz@abv.bg>
2021-08-03 14:04:10 +0300

    moved raising error views from urls.py to views.py

--- Picturedom/Picturedom/views.py
+++ Picturedom/Picturedom/views.py
@@ -1,5 +1,9 @@
+from django.core.exceptions import SuspiciousOperation, PermissionDenied
+from django.http import Http404, HttpResponseServerError
 from django.shortcuts import render
 
+
+# Custom Handlers
 
 def error_400_view(request, exception):
     return render(request, 'errors/400.html', status=400)
@@ -15,3 +19,21 @@ def error_404_view(request, exception):
 
 def error_500_view(request):
     return render(request, 'errors/500.html', status=500)
+
+
+# Raising error
+
+def raise_error_400_bad_request(request):
+    raise SuspiciousOperation
+
+
+def raise_error_403_forbidden(request):
+    raise PermissionDenied
+
+
+def raise_error_404_page_not_found(request):
+    raise Http404('Exception')
+
+
+def raise_error_500_server_error(request):
+    raise HttpResponseServerError
