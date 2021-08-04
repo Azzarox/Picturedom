341c202fbba55238c3d4870c34a75e9855aebc9f
azzarox <lenovo700gmz@abv.bg>
2021-08-04 10:16:12 +0300

    debug settings

--- Picturedom/Picturedom/settings.py
+++ Picturedom/Picturedom/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--l!i*%*@nk!bz*#-06nzkasuepqwm16fj)y&eh&rn&w$%$+2+6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
