b40c00a963bceec5abdfc4456f802bd32f13441c
Azzarox <62171309+Azzarox@users.noreply.github.com>
2025-01-26 15:08:21 +0200

    Updated to merge

--- Picturedom/Picturedom/settings.py
+++ Picturedom/Picturedom/settings.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = 'Picturedom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'), 
-        'USER': config('DB_USER'), 
-        'PASSWORD': config('DB_PASS'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': config('DB_NAME'), 
+        # 'USER': config('DB_USER'), 
+        # 'PASSWORD': config('DB_PASS'),
+        # 'HOST': config('DB_HOST'),
+        # 'PORT': config('DB_PORT'),
     }
 }
 
