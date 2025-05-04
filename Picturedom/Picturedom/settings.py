2409ec785a10338cb9facffcc03fa9978a23ce08
Azzarox <62171309+Azzarox@users.noreply.github.com>
2025-05-04 16:43:53 +0300

    Added cloudinary

--- Picturedom/Picturedom/settings.py
+++ Picturedom/Picturedom/settings.py
@@ -50,6 +50,21 @@ INSTALLED_APPS = [
 
 ]
 
+# Add cloudinary to Prod Branch
+INSTALLED_APPS += [
+    'cloudinary',
+    'cloudinary_storage'
+]
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY':    config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
+
+# 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
